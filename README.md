ddf45cc99cc43fe350e9e3a5fe223781b2cec6ac
Patrick Cookson <cookson.pat@gmail.com>
2026-02-19 21:22:03 -0500

    created mono repo with docker

---
+++ README.md
@@ -0,0 +1,12 @@
+# AstroSignals
+
+## Run
+
+```bash
+docker-compose up --build
+```
+
+## Open
+
+- Web: http://localhost:5173
+- API health: http://localhost:8000/api/health
---
+++ apps/web/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>AstroSignals</main>
+</template>
+
+<style scoped>
+main {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  font-family: sans-serif;
+  font-size: 2rem;
+}
+</style>
